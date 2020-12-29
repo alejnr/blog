97eeeb1304d9ae0594e6198d3a51427fbd1d076a
Alex Menlah <a.menlah@itebodoni.edu.it>
2020-12-29 21:24:43 +0100

    rendering data from database

--- app.js
+++ app.js
@@ -1,42 +1,95 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
 const ejs = require('ejs')
 const _ = require('lodash')
 
-const homeStartingContent =
-  'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.'
-
-const aboutContent =
-  'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.'
-const contactContent =
-  'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.'
-
 const app = express()
 const port = 3000
-
-const posts = []
 
 app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true})
+
+const defaultContentSchema = new mongoose.Schema({
+  name: String,
+  content: String
+})
+
+const DefaultContent = mongoose.model('DefaultContent', defaultContentSchema)
+
+const homeStartingContent = new DefaultContent({
+  name: 'homeStartingContent',
+  content: 'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.'
+})
+
+const aboutContent = new DefaultContent({
+  name: 'aboutContent',
+  content: 'Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.'
+})
+
+const contactContent = new DefaultContent({
+  name: 'contactContent',
+  content: 'Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.'
+})
+
+const defaultContents = [homeStartingContent, aboutContent, contactContent]
+
+
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Please check your data entry, no title specified!']
+  },
+  content:  {
+    type: String,
+    required: [true, 'Please check your data entry, no post specified!']
+  }
+})
+
+const Post = mongoose.model('Post', blogSchema)
+
+
 app.get('/', function (req, res) {
   const kebabCase = _.kebabCase
 
-  res.render('home', {
-    startingContent: homeStartingContent,
-    newPost: posts,
-    kebabCase: kebabCase,
+  DefaultContent.find(function(err, contents) {
+
+    if (contents.length === 0) {
+      DefaultContent.insertMany(defaultContents, function(err) {
+            if (err) {
+              console.log(err);
+            } else {
+                res.redirect('/')
+            }
+          })
+    } else {
+
+      Post.find({}, function (err, newPost) {
+
+        res.render('home', {
+          startingContent: homeStartingContent.content,
+          newPost: newPost,
+          kebabCase: kebabCase,
+        })
+      })
+      
+    }
+    
   })
+
 })
 
 app.get('/about', function (req, res) {
-  res.render('about', { aboutContent: aboutContent })
+  res.render('about', { aboutContent: aboutContent.content })
 })
 
 app.get('/contact', function (req, res) {
-  res.render('contact', { contactContent: contactContent })
+  res.render('contact', { contactContent: contactContent.content })
 })
 
 app.get('/compose', function (req, res) {
@@ -60,7 +113,14 @@ app.post('/compose', function (req, res) {
     title: req.body.postTitle,
     content: req.body.postBody,
   }
-  posts.push(post)
+
+  const newPost = new Post({
+    title: post.title,
+    content: post.content
+  })
+
+  newPost.save()
+
   res.redirect('/')
 })
 
